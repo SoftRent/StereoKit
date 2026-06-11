81565bbeba9ed07b8cedc5c95ebb4162e4f350d6
SoftRent <codewithneerb@gmail.com>
2026-06-11 18:46:17 +0530

    Initial commit

---
+++ StereoKitC/shaders_builtin/shader_builtin.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "shader_builtin_skybox.hlsl.h"
+#include "shader_builtin_pbr.hlsl.h"
+#include "shader_builtin_pbr_clip.hlsl.h"
+#include "shader_builtin_default.hlsl.h"
+#include "shader_builtin_blit.hlsl.h"
+#include "shader_builtin_unlit.hlsl.h"
+#include "shader_builtin_unlit_clip.hlsl.h"
+#include "shader_builtin_lightmap.hlsl.h"
+#include "shader_builtin_equirect.hlsl.h"
+#include "shader_builtin_blit.hlsl.h"
+#include "shader_builtin_font.hlsl.h"
+#include "shader_builtin_lines.hlsl.h"
+#include "shader_builtin_ui.hlsl.h"
+#include "shader_builtin_ui_box.hlsl.h"
+#include "shader_builtin_ui_quadrant.hlsl.h"
+#include "shader_builtin_ui_aura.hlsl.h"
