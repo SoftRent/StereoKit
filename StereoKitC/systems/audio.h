b15bbba697b7cef5702607a4ba6e2ee2784073dd
SoftRent <codewithneerb@gmail.com>
2026-06-11 18:46:51 +0530

    Initial commit

---
+++ StereoKitC/systems/audio.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "../stereokit.h"
+
+namespace sk {
+
+#define AU_SAMPLE_RATE   48000
+#define AU_SAMPLE_FORMAT ma_format_f32
+
+struct _sound_inst_t {
+	sound_t  sound;
+	uint16_t id;
+	vec3     position;
+	float    volume;
+};
+
+bool audio_init    ();
+void audio_step    ();
+void audio_shutdown();
+
+void audio_pause ();
+void audio_resume();
+
+#if defined(SK_OS_WINDOWS) || defined(SK_OS_WINDOWS_UWP)
+void audio_set_default_device_in (const wchar_t *id);
+void audio_set_default_device_out(const wchar_t *id);
+#endif
+
+extern _sound_inst_t au_active_sounds[8];
+
+} // namespace sk
