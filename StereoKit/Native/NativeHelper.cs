2399dd20ef93203cf8a12ef9ae737ea7dc1337d4
SoftRent <codewithneerb@gmail.com>
2026-06-11 18:42:54 +0530

    Initial commit

---
+++ StereoKit/Native/NativeHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace StereoKit
+{
+	internal class NativeHelper
+	{
+		public static string FromUtf8(IntPtr str, int strLength)
+		{
+			string result = null;
+			if (str != IntPtr.Zero)
+			{
+				byte[] filenameUtf8 = new byte[strLength];
+				Marshal.Copy(str, filenameUtf8, 0, strLength);
+
+				int terminator = Array.IndexOf(filenameUtf8, (byte)0);
+				result = Encoding.UTF8.GetString(filenameUtf8, 0, terminator == -1 ? strLength : terminator);
+			}
+			return result;
+		}
+
+		public static string FromUtf8(IntPtr str)
+			=> FromUtf8(str, Marshal.PtrToStringAnsi(str).Length);
+
+		public static byte[] ToUtf8(string str)
+			=> str == null ? null : Encoding.UTF8.GetBytes(str + '\0');
+	}
+}
