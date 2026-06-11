f632e8b6e97099dfa4292a19d9ab9b0c62de5a1a
SoftRent <codewithneerb@gmail.com>
2026-06-11 18:43:26 +0530

    Initial commit

---
+++ StereoKit/Util/DefaultIds.cs
@@ -0,0 +1,58 @@
+﻿namespace StereoKit {
+	internal static class DefaultIds
+	{
+		internal const string material           = "default/material";
+		internal const string materialPBR        = "default/material_pbr";
+		internal const string materialPBRClip    = "default/material_pbr_clip";
+		internal const string materialUnlit      = "default/material_unlit";
+		internal const string materialUnlitClip  = "default/material_unlit_clip";
+		internal const string materialEquirect   = "default/equirect_convert";
+		internal const string materialFont       = "default/material_font";
+		internal const string materialHand       = "default/material_hand";
+		internal const string materialUI         = "default/material_ui";
+		internal const string materialUIBox      = "default/material_ui_box";
+		internal const string materialUIQuadrant = "default/material_ui_quadrant";
+
+		internal const string tex      = "default/tex";
+		internal const string texBlack = "default/tex_black";
+		internal const string texGray  = "default/tex_gray";
+		internal const string texFlat  = "default/tex_flat";
+		internal const string texRough = "default/tex_rough";
+		internal const string texDevTex= "default/tex_devtex";
+		internal const string texError = "default/tex_error";
+		internal const string cubemap  = "default/cubemap";
+
+		internal const string font = "default/font";
+
+		internal const string meshQuad       = "default/mesh_quad";
+		internal const string meshScreenQuad = "default/mesh_screen_quad";
+		internal const string meshCube       = "default/mesh_cube";
+		internal const string meshSphere     = "default/mesh_sphere";
+
+		internal const string shader         = "default/shader";
+		internal const string shaderPbr      = "default/shader_pbr";
+		internal const string shaderPbrClip  = "default/shader_pbr_clip";
+		internal const string shaderUnlit    = "default/shader_unlit";
+		internal const string shaderUnlitClip= "default/shader_unlit_clip";
+		internal const string shaderFont     = "default/shader_font";
+		internal const string shaderEquirect = "default/shader_equirect";
+		internal const string shaderUI       = "default/shader_ui";
+		internal const string shaderUIBox    = "default/shader_ui_box";
+		internal const string soundClick     = "default/sound_click";
+		internal const string soundUnclick   = "default/sound_unclick";
+
+		internal const string spriteRadioOn    = "sk/ui/radio_on";
+		internal const string spriteRadioOff   = "sk/ui/radio_off";
+		internal const string spriteToggleOn   = "sk/ui/toggle_on";
+		internal const string spriteToggleOff  = "sk/ui/toggle_off";
+		internal const string spriteArrowLeft  = "sk/ui/arrow_left";
+		internal const string spriteArrowRight = "sk/ui/arrow_right";
+		internal const string spriteArrowUp    = "sk/ui/arrow_up";
+		internal const string spriteArrowDown  = "sk/ui/arrow_down";
+		internal const string spriteBackspace  = "sk/ui/backspace";
+		internal const string spriteShift      = "sk/ui/shift";
+		internal const string spriteClose      = "sk/ui/close";
+		internal const string spriteList       = "sk/ui/list";
+		internal const string spriteGrid       = "sk/ui/grid";
+	}
+}
